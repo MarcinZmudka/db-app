c4229ffd570ac4947d151496234061ed3e5892a4
unknown <marcinzmudka@gmail.com>
2019-12-15 21:34:17 +0100

    connection to azure added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,36 @@
-import express from 'express';
+import express from "express";
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.use(cors());
+const port = 3001;
+const sql = require("mssql");
+const config = {
+  user: "adminazurebaza",
+  password: "Test12345",
+  server: "azuretestbazy.database.windows.net",
+  database: "TEST",
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true
+  }
+};
+
+app.get("/", async function(req, res) {
+  try {
+    const pool = await sql.connect(config);
+    const response = await pool.request().query(`select * from users`);
+    res.send(response);
+    await sql.close();
+  } catch (err) {
+    console.log(err);
+  }
 });
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
